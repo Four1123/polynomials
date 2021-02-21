a91beee5b33b03cf1d22fc1a34e1d9f9195050a6
Four1123 <wl1919@imperial.ac.uk>
2021-02-21 23:52:00 +0800

    Basic polynomial implementation

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -12,12 +12,21 @@ class Polynomial:
         coefs = self.coefficients
         terms = []
 
+        # Degree 0 and 2 terms conventionally have different representation.
         if coefs[0]:
             terms.append(str(coefs[0]))
         if self.degree() and coefs[1]:
             terms.append(f"{'' if coefs[1] == 1 else coefs[1]}x")
 
+        # Remaining terms look like cx^d, though factors of 1 are dropped.
         terms += [f"{'' if c == 1 else c}x^{d}"
                   for d, c in enumerate(coefs[2 : ], start = 2) if c]
 
+        # Sum polynomial terms from high to low exponent.      
         return ' + '.join(reversed(terms)) or '0'
+
+ 
+    def __repr__(self):
+
+        return self.__class__.__name__ + "(" + repr(self.coefficients) + ")"
+
